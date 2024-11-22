928d4ef0541a400f5cf5154032ce8c61e43452d2
EsmeeWestdijk <145474048+EsmeeWestdijk@users.noreply.github.com>
2024-11-22 11:46:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("hi");
+
+const menuButton = document.querySelector("nav Button");
+const deNav = document.querySelector("nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+menuButton.onclick = toggleMenu; 
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu(){
+  deNav.classList.toggle("toonMenu"); 
+}
+
+// bron: sanne les stof 
