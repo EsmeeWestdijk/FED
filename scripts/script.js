d8bf1aaee6b3988995dbe3df5780b1816c04b5b2
EsmeeWestdijk <145474048+EsmeeWestdijk@users.noreply.github.com>
2024-12-05 22:26:31 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,25 @@
-// JavaScript Document
-var openButton = document.querySelector("header > button");
-openButton.onclick = openMenu;
+const btnItem = document.querySelector('header nav img:nth-of-type(2)')
+const menuItem = document.querySelector('header nav:nth-of-type(2)')
+const bodyEl = document.body 
+const closeBtn = document.querySelector("header nav:nth-of-type(2) div button")
+const thumbnail = document.querySelector("main>section:nth-of-type(6) li img:first-of-type")
+const video = document.querySelector("main>section:nth-of-type(6) video")
 
-function openMenu() {  
-  var deNav = document.querySelector("nav");
-  deNav.classList.add("toonMenu");
+
+btnItem.addEventListener('click', openMenu)
+closeBtn.addEventListener("click", closeMenu)
+thumbnail.addEventListener("click", showVideo)
+
+//Menu//
+function openMenu(){
+  menuItem.classList.add('active')
+  bodyEl.classList.add('inactive')
+
 }
 
-
-
-
-/************************************/
-/* menu sluiten met de sluit button */
-/************************************/
-
-
-var sluitButton = document.querySelector("nav button");
-sluitButton.onclick = sluitMenu;
-
-function sluitMenu() {
-  var deNav = document.querySelector("nav");
-  deNav.classList.remove("toonMenu");
+function closeMenu(){
+  menuItem.classList.remove('active')
+  bodyEl.classList.remove('inactive')
 }
+
+//Video//
